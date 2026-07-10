95f13a60abf3077a88759b176cdab46115261706
Pedro <pedrojosecavudissaao@gmail.com>
2026-07-10 12:11:20 +0100

    w

--- hotel-pms/vite.config.ts
+++ hotel-pms/vite.config.ts
@@ -1,27 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url' // 1. Importa esta ferramenta nativa
+import { fileURLToPath } from 'url'
 
-// 2. Cria o equivalente ao __dirname de forma compatível com ES Modules
+// Cria o equivalente ao __dirname de forma compatível com ES Modules
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    // Agora o path.resolve(__dirname) vai funcionar perfeitamente e sem erros!
-    alias: { 
-      '@': path.resolve(__dirname, './src') 
+    // Atalho elegante para importar arquivos usando '@/'
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  server: { 
-    proxy: { 
-      '/api': { 
-        target: 'http://backend-app:9090',
-        changeOrigin: true, 
-        rewrite: (p) => p.replace(/^\/api/, '') 
-      } 
-    } 
-  },
+  server: {
+    // Escuta em todas as interfaces de rede (essencial para o Docker e Dokploy)
+    host: '0.0.0.0',
+    
+    // Define a tua porta de produção/teste para o Frontend
+    port: 9091,
+    
+    // Permite que o Ngrok, sslip.io ou qualquer domínio aceda sem o erro "Invalid Host"
+    allowedHosts: true      
+  }
 })
